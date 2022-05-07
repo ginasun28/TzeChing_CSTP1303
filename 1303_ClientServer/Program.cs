5ebec5fb5443ed9f7193f3a676ddb0160a53a2e1
000448992 <000448992@CC145XX1A026XXX>
2022-05-06 19:05:53 -0700

    Add project files.

---
+++ 1303_ClientServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1303_ClientServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
